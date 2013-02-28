909afff829b6019535401a15945c2dd65761d1f3
TJ Holowaychuk <tj@vision-media.ca>
2013-02-27 19:15:37 -0800

    add logo rendering

--- browser.js
+++ browser.js
@@ -1,4 +1,11 @@
 
+/**
+ * Module dependencies.
+ */
+
+var Canvas = require('canvas')
+  , Image = Canvas.Image;
+
 module.exports = Browser;
 
 function Browser(name) {
@@ -22,4 +29,13 @@ Browser.prototype.add = function(type, version){
   });
 
   return this;
+};
+
+Browser.prototype.draw = function(ctx, options){
+  var size = options.size;
+  var fontSize = options.fontSize;
+  var img = new Image;
+  img.src = this.image;
+
+  ctx.drawImage(img, 0, 0, size, size);
 };
--- index.js
+++ index.js
@@ -11,8 +11,8 @@ module.exports = Badge;
 
 function Badge() {
   this.browsers = [];
-  this.columnWidth = 50;
-  this.versionHeight = 15;
+  this.columnWidth = 100;
+  this.versionHeight = 25;
 }
 
 Badge.prototype.width = function(){
@@ -31,7 +31,24 @@ Badge.prototype.browser = function(name){
 };
 
 Badge.prototype.render = function(){
-  var canvas = new Canvas(this.width(), this.height());
-  this.draw(canvas.getContext('2d'));
+  var self = this;
+  var w = this.width();
+  var h = this.height();
+  var canvas = new Canvas(w, h);
+  var ctx = canvas.getContext('2d');
+  var size = this.columnWidth;
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, w, h);
+
+  this.browsers.forEach(function(b){
+    b.draw(ctx, {
+      size: size,
+      fontSize: self.versionHeight
+    });
+
+    ctx.translate(size, 0);
+  })
+
   return canvas;
 };
